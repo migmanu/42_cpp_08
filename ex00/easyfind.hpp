dda33e44926c4d05f6daa0086cfe3fd0e1a31845
migmanu <jmanuelmigoya@gmail.com>
2024-06-14 12:04:59 +0200

    feat: finish ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,22 +6,23 @@
 /*   By: migmanu </var/spool/mail/migmanu>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 15:42:17 by migmanu           #+#    #+#             */
-/*   Updated: 2024/06/10 20:06:56 by migmanu          ###   ########.fr       */
+/*   Updated: 2024/06/14 11:47:04 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <algorithm>
-#include <exception>
 #include <iostream>
+#include <sstream>
 #include <stdexcept>
 #include <vector>
 
-template <typename T> std::vector<int>::iterator easyfind(T &container, int target)
+template <typename T>
+void easyfind(T &container, int target)
 {
-	typename T::value_type s;
-	std::cout << s << std::endl;
-	std::vector<int>::iterator r = std::find(container.begin(), container.end(), target);
-	if (r == container.end())
-		throw std::invalid_argument("easyfind : target not found in container");
-	return r;
+	if (std::find(container.begin(), container.end(), target) != container.end())
+		std::cout << "Target '" << target << "' found in container" << std::endl;
+	else
+		std::cout << "Target '" << target << "' NOT found in container" << std::endl;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,27 +6,34 @@
 /*   By: migmanu </var/spool/mail/migmanu>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 17:42:19 by migmanu           #+#    #+#             */
-/*   Updated: 2024/06/10 19:59:50 by migmanu          ###   ########.fr       */
+/*   Updated: 2024/06/14 11:44:49 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
-#include <exception>
+#include <cstdlib>
+#include <deque>
 #include <iostream>
+#include <list>
 #include <vector>
 
 int main(void)
 {
-	std::vector<int> integers(9, 4);
+	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int target = -5;
 
-	integers.at(1) = 2;
-	try {
-		std::vector<int>::iterator r =  easyfind(integers, 2);
-		int f = *r;
-		std::cout << "r:" << *r << std::endl;
-		std::cout << "f:" << f << std::endl;
-	} catch (std::exception &e) {
-	
-	}
+	std::vector<int> vec(arr, arr + sizeof(arr) / sizeof(int));
+	std::deque<int> dq(arr, arr + sizeof(arr) / sizeof(int));
+	std::list<int> lst(arr, arr + sizeof(arr) / sizeof(int));
+
+	std::cout << "Test vector" << std::endl;
+	easyfind(vec, target);
+
+
+	std::cout << "Test deque" << std::endl;
+	easyfind(dq, target);
+
+	std::cout << "Test list" << std::endl;
+	easyfind(lst, target);
 	return 0;
 }
